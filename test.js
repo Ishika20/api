0e3522803e88c5eea54c728d6164d53880121540
Ishika20 <31150548+Ishika20@users.noreply.github.com>
2018-10-26 11:14:20 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+function onSignIn(googleUser){
+	var profile = googleUser.getBasicProfile();
+	$("#data").css("visibility" , "visible");
+	$("#logout").css("visibility" , "hidden");
+	$(".g-signin2").css("display" , "none");
+	$(".name").text(profile.getName());
+	$("#myImg").attr("src",profile.getImageUrl());
+	$("#email").text(profile.getEmail());
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      $(".g-signin2").css("display" , "block");
+      $("#data").css("visibility" , "hidden");
+      $("#logout").css("visibility" , "visible");
+       });
+  
